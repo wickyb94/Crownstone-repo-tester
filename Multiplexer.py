f6c7012761bb8b80487ac9a64123d89617dcb5e7
wickyb94 <wicky_bhaggoe@hotmail.com>
2020-03-16 22:34:22 +0100

    testen met meerdere led

--- Multiplexer.py
+++ Multiplexer.py
@@ -1,21 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 
-S0 = 6
-S1 = 13
-S2 = 19
-S3 = 26
-sic = 21
+# dit zijn de pinnen S0,S1,S2,S3,sic op de multiplexer 
+mp = [6,13,19,26,21]  
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(S0, GPIO.OUT)
-GPIO.setup(S1, GPIO.OUT)
-GPIO.setup(S2, GPIO.OUT)
-GPIO.setup(S3, GPIO.OUT)
-GPIO.setup(sic, GPIO.OUT)
+for setting in range(0,5):
+    GPIO.setup(mp[setting],GPIO.OUT)
 
 def multi(channel):
-    controlPin = [S0, S1, S2, S3] 
+    controlPin = [mp[0],mp[1],mp[2],mp[3]] 
     muxChannel = [
         [0,0,0,0],
         [1,0,0,0],
@@ -38,18 +32,14 @@ def multi(channel):
     for i in range(0,4):
         GPIO.output(controlPin[i],muxChannel[channel][i])
 
-GPIO.output(sic,GPIO.HIGH)
+GPIO.output(mp[4],GPIO.HIGH)
 
-
-
-multi(2)
-print( "test 1")
-time. sleep(3)
-multi(14)
-time.sleep(3)
-print ("test 2")
-
-
-
+for x in range(8,16):
+    multi(x)
+    print( "test")
+    time. sleep(0.1)
+#multi(12)
+#time.sleep(3)
+#print ("test 2")
 
 GPIO.cleanup()
---
+++ relaisiopi.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+from IOPi import IOPi
+import time
+
+bus = IOPi(0x21)
+bus.set_port_direction(0, 0x00)
+bus.set_port_direction(1, 0x00)
+bus.write_port(0, 0xFF)
+bus.write_port(1, 0xFF)
+
+time.sleep(1)
+
+for x in range (1,17):
+    bus.write_pin(x, 0)
+    time.sleep(0.5)
