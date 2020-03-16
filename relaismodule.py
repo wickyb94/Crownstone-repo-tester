2a106842cf25987cb2e3b8fcb60b35915e6c3c26
wickyb94 <wicky_bhaggoe@hotmail.com>
2020-03-16 22:28:54 +0100

    Add files via upload

--- relaismodule.py
+++ relaismodule.py
@@ -1,41 +1,19 @@
 import RPi.GPIO as GPIO
 import time
 
-// bla
-een  = 6
-twee = 13
-drie = 19
-vier = 26
-vijf = 21
+rel = [6, 13, 19, 26] 
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(een, GPIO.OUT)
-GPIO.setup(twee, GPIO.OUT)
-GPIO.setup(drie, GPIO.OUT)
-GPIO.setup(vier, GPIO.OUT)
-GPIO.setup(vijf, GPIO.OUT)
+for seting in range(0,4):
+    GPIO.setup(rel[seting],GPIO.OUT)
 
-GPIO.output(een,GPIO.HIGH)
-GPIO.output(twee,GPIO.HIGH)
-GPIO.output(drie,GPIO.HIGH)
-GPIO.output(vier,GPIO.HIGH)
+for x in range(0,4):
+    GPIO.output(rel[x],GPIO.HIGH)
 
-GPIO.output(een,GPIO.LOW)
-print( "test 1")
-time.sleep(3)
-GPIO.output(twee,GPIO.LOW)
-print( "test 1")
-time.sleep(3)
-GPIO.output(drie,GPIO.LOW)
-print( "test 1")
-time.sleep(3)
-GPIO.output(vier,GPIO.LOW)
-print( "test 1")
-time.sleep(3)
-
-
-
-
+for y in range(0,4):
+    GPIO.output(rel[y],GPIO.LOW)
+    print( "test")
+    time.sleep(3)
 
 
 GPIO.cleanup()
