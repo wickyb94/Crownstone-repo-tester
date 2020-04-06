86c42ec06bdc3cc71e501e3fe08e73a89fa90c22
wickyb94 <wicky_bhaggoe@hotmail.com>
2020-04-06 21:09:32 +0200

    invoegen van subprocessen

--- Multiplexer.py
+++ Multiplexer.py
@@ -1,9 +1,10 @@
 import RPi.GPIO as GPIO
 import time
+import subprocess
 
 
-# dit zijn de pinnen S0,S1,S2,S3,sic op de multiplexer 
-mp = [6,13,19,26,21]  
+# dit zijn de pinnen S0,S1,S2,S3,EN op de multiplexer 
+mp = [6,13,19,26,20]  
 
 GPIO.setmode(GPIO.BCM)
 
@@ -30,18 +31,26 @@ def multi(channel):
         [0,1,1,1],
         [1,1,1,1],
     ]
-    
+    GPIO.output(mp[4],1)
     for i in range(0,4):
         GPIO.output(controlPin[i],muxChannel[channel][i])
+    GPIO.output(mp[4],0)
 
-GPIO.output(mp[4],GPIO.HIGH)
+inp = 1
+while inp == 1:
+    multi(0)
+    print(inp)
+    inp = input()
+print(inp)
+#for x in range(0,16):
+#    multi(x)
+#    print(x)
+#    time. sleep(0.5)
+#multi(1)
 
-for x in range(0,16):
-    multi(x)
-    print(x)
-    time. sleep(1)
+#dir = '/home/pi/bestanden/JLink_Linux_V664_arm/JLinkExe -Device NRF52832_XXAA -speed 400 -if SWD -autoconnect 1'
+#s1 = subprocess.run(dir, shell= True)
 
-#multi(15)
 #time.sleep(1)
 #print ("test 2")
 
