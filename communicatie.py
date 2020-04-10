f232cac52e09f4a922fa4d26398d8c40704a66db
wickyb94 <wicky_bhaggoe@hotmail.com>
2020-04-10 17:25:32 +0200

    connect met swd en het versturen van commandos

--- communicatie.py
+++ communicatie.py
@@ -1,7 +1,7 @@
 import subprocess
 import time
 from jlink import *
-
+'''
 cjlink = ['/opt/JLink_Linux_V664_arm/JLinkExe', '-Device', 'NRF52832_XXAA', '-speed', '4000', '-if', 'SWD', '-autoconnect', '1']
 flash_command = cjlink + ['i']
 session = subprocess.Popen(cjlink, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -11,16 +11,14 @@ for line in iter(stdout):
     if line.decode('utf-8') == '':
         break
     print(line.decode('utf-8'))
-    print('hoi')
 
 if session.returncode != 0:
     print("FAILED!")
 
 print(session.returncode)
 
-
-#ob = Programmer()
-#commands = JLink(ob)
-#x = commands.run_commands(['st','device nRF52832_XXAA','SelectInterface SWD','speed 1000','i','exit'], 0)
-#print (x)
-
+'''
+ob = Programmer()
+commands = JLink(ob)
+jlinkout = commands.run_commands(['connect','i','exit'], 4)
+print (jlinkout)
