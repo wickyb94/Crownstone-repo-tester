8464488705c049fd0b4d6b07ba44fe7116637bb9
wickyb94 <wicky_bhaggoe@hotmail.com>
2020-04-10 12:40:46 +0200

    invoegen van subprocessen

--- communicatie.py
+++ communicatie.py
@@ -5,16 +5,19 @@ from jlink import *
 #cjlink = ['/opt/JLink_Linux_V664_arm/JLinkExe', '-Device', 'NRF52832_XXAA', '-speed', '4000', '-if', 'SWD', '-autoconnect', '1']
 #flash_command = cjlink + ['i']
 #session = subprocess.Popen(cjlink, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#time.sleep(5)
-#print(session.returncode)
 #stdout = session.communicate()
 
+#for line in iter(stdout):
+#    if line.decode('utf-8') == '':
+#        break
+#    print(line.decode('utf-8'))
+#    print('hoi')
 
 if session.returncode != 0:
     print("FAILED!")
 
 #print(session.returncode)
 
-
+#run_commands(self,'connect',60)
 a=JLink('NRF52832_XXAA')
 a.run_commands('i',60)
