c459e3f552c8f45e82316ad13fc3b52dd47329ca
wickyb94 <wicky_bhaggoe@hotmail.com>
2020-04-13 22:54:29 +0200

    zie mail

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from jlink import *
 from relaisiopi import *
 from multiplexer import *
+a = 1
+while a == 1:
+    bus.write_pin(1,0)
+    multi(5)
+    time.sleep(0.5)
 
-bus.write_pin(1 , 0)
-multi(5)
-ob = Programmer()
-commands = JLink(ob)
-jlinktupp = commands.run_commands(['connect','i','exit'], 4)
-jlinkout = jlinktupp.split('3.300V')[1]
-print (jlinktupp)
-bus.write_pin(1 , 1)
+    ob = Programmer()
+    commands = JLink(ob)
+    jlinktupp = commands.run_commands(['connect','i','mem32 0 10','exit'], 4)
+    jlinkout = jlinktupp.split('3.300V')[1]
+    #x = commands.readmem32()
+    print (jlinkout)
+
+    a = input()
+    bus.write_pin(1 , 1)
+    time.sleep(1)
+
+
 
 GPIO.cleanup()
