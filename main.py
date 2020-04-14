fb5b0502f315305075e2a00eeff74a01bb9469c6
wickyb94 <wicky_bhaggoe@hotmail.com>
2020-04-14 21:40:01 +0200

    testen uart en multiplexers

--- main.py
+++ main.py
@@ -4,19 +4,17 @@ from multiplexer import *
 a = 1
 while a == 1:
     bus.write_pin(1,0)
-    multi(5)
+    multi(1)
     time.sleep(0.5)
 
     ob = Programmer()
     commands = JLink(ob)
     jlinktupp = commands.run_commands(['connect','i','mem32 0 10','exit'], 4)
     jlinkout = jlinktupp.split('3.300V')[1]
-    #x = commands.readmem32()
     print (jlinkout)
-
     a = input()
-    bus.write_pin(1 , 1)
-    time.sleep(1)
+bus.write_pin(1 , 1)
+time.sleep(1)
 
 
 
