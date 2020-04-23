052082bd09e5984ea283beb84757885af79979f3
wickyb94 <wicky_bhaggoe@hotmail.com>
2020-04-23 13:02:55 +0200

    functie setrelay toegevoegd

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ multiplex(1)
 #using a function from jlink.py to run a few command when doing that the first command must be connect
 ob = Programmer()
 commands = JLink(ob)
-jlinktupp= commands.run_commands(['connect','i','mem32 0 10','exit'], 4)
+jlinktupp = commands.run_commands(['connect','i','mem32 0 10','exit'], 4)
 jlinkout = jlinktupp.split('3.300V')[1]
 print (jlinkout)
 '''
