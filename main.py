0159c7bb1161e9674c940714a1785f8b72d7b464
wickyb94 <wicky_bhaggoe@hotmail.com>
2020-06-04 01:57:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,42 +5,42 @@ from uarttest import *
 import math
 
 #The main script excecutes the tests and functions of the other scripts
-# These are the pins S0,S1,S2,S3,EN of the multiplexer connected to the Raspberry PI
+# initializing the pins of the multiplexer these pins are S0,S1,S2,S3,EN of the multiplexer
 mp = [6, 13, 19, 26, 21]
+GPIO.setmode (GPIO.BCM)
+for setting in range (0, 5):
+    GPIO.setup (mp[setting], GPIO.OUT)
 
-GPIO.setmode(GPIO.BCM)
-for setting in range(0, 5):
-    GPIO.setup(mp[setting], GPIO.OUT)
+#intitializing the usb port wich will be used for the bluenet library 
+bluenet.initializeUSB ("/dev/ttyUSB0")
 
-bluenet.initializeUSB("/dev/ttyUSB0")
-
-ob = Programmer()
-commands = JLink(ob)
+ob = Programmer ()
+commands = JLink (ob)
 multiplexers = []
-crownstoneCount = 3
+crownstoneCount = 2
 crownstonePerMultiplexer = 16
 
 multiplexer1 = Multiplexer (mp[0], mp[1], mp[2], mp[3], mp[4])
-multiplexers.append(multiplexer1)
+multiplexers.append (multiplexer1)
 
-for select in range(1, crownstoneCount):
-    setRelayOn(select)
+for select in range (1, crownstoneCount+1):
+    setRelayOn (select)
     
-    multiplexerId = math.floor(select / crownstonePerMultiplexer)
+    multiplexerId = math.floor (select / crownstonePerMultiplexer)
     multiplexers[multiplexerId].switch (select)
 
-    jlinkTupp = commands.run_filename('/home/pi/firmware-tester/loaddevfirmware.script', 20)
-    jlinkOut = jlinkTupp.split('3.300V')[2]
-    succesCount = jlinkTupp.count('100%')
-    print(jlinkOut)
+    jlinkTupp = commands.run_filename ('/home/pi/firmware-tester/loaddevfirmware.script', 20)
+    jlinkOut = jlinkTupp.split ('3.300V')[2]
+    succesCount = jlinkTupp.count ('100%')
+    print (jlinkOut)
     print (succesCount)
-    print('update is done ')
+    print ('update is done ')
     time.sleep(1)
 
-    print(getMacAddress())
+    print (getMacAddress())
 
-    setRelayOff(select)
+    setRelayOff (select)
 
 
-bluenet.stop()
-GPIO.cleanup()
+bluenet.stop ()
+GPIO.cleanup ()
