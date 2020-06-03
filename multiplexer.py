bc178d72a058b61048c035ea1b542affaec0404f
wickyb94 <wicky_bhaggoe@hotmail.com>
2020-06-03 22:44:14 +0200

    Update multiplexer.py

--- multiplexer.py
+++ multiplexer.py
@@ -1,44 +1,49 @@
 import RPi.GPIO as GPIO
 import time
 
-def multiplexerErrors(errornumber):
-    if errornumber == 1:
-        print('input out of bound input must be an integer from 1 to 16')
+class Multiplexer:
+    muxChannel = [
+                [0, 0, 0, 0],
+                [1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [1, 1, 0, 0],
+                [0, 0, 1, 0],
+                [1, 0, 1, 0],
+                [0, 1, 1, 0],
+                [1, 1, 1, 0],
+                [0, 0, 0, 1],
+                [1, 0, 0, 1],
+                [0, 1, 0, 1],
+                [1, 1, 0, 1],
+                [0, 0, 1, 1],
+                [1, 0, 1, 1],
+                [0, 1, 1, 1],
+                [1, 1, 1, 1],
+            ]
+    
+    def __init__ (self, S0, S1, S2, S3, EN):
+        self.S0 = S0
+        self.S1 = S1
+        self.S2 = S2
+        self.S3 = S3
+        self.EN = EN
 
-#function where you can decide wich channel is connected. It ranges from 1 to 16
-def multiplex(channel, S0, S1, S2, S3, EN):
-    channel = channel-1
-    if channel > -1 and channel < 17:
-        controlPin = [S0, S1, S2, S3]
-        muxChannel = [
-            [0, 0, 0, 0],
-            [1, 0, 0, 0],
-            [0, 1, 0, 0],
-            [1, 1, 0, 0],
-            [0, 0, 1, 0],
-            [1, 0, 1, 0],
-            [0, 1, 1, 0],
-            [1, 1, 1, 0],
-            [0, 0, 0, 1],
-            [1, 0, 0, 1],
-            [0, 1, 0, 1],
-            [1, 1, 0, 1],
-            [0, 0, 1, 1],
-            [1, 0, 1, 1],
-            [0, 1, 1, 1],
-            [1, 1, 1, 1],
-        ]
-        GPIO.output(EN, 1)
-        for i in range(0, 4):
-            GPIO.output(controlPin[i], muxChannel[channel][i])
-        GPIO.output(EN, 0)
-        time.sleep(0.5)
-    else:
-        multiplexerErrors(1)
+    #function where you can decide wich channel is connected. It ranges from 1 to 16
+    def switch (self, channel):
+        channel = channel-1
+        if channel > -1 and channel < 17:
+            controlPin = [self.S0, self.S1, self.S2, self.S3]
 
-def testalloutputs(S0, S1, S2, S3, EN):
-    for channelnumber in range(1, 16):
-        multiplex(channelnumber, S0, S1, S2, S3, EN)
-        print(channelnumber)
-        time.sleep(0.1)
+            GPIO.output(self.EN, 1)
+            for i in range(0, 4):
+                GPIO.output(controlPin[i], muxChannel[channel][i])
+            GPIO.output(self.EN, 0)
+        else:
+            raise Exception ('Channel number out of bounds, channel must be an integer from 1 to 16")
+
+    def testAllOutputs(self):
+        for channel in range(1, 16):
+            self.switch(channel)
+            print(channel)
+            time.sleep(0.1)
 
