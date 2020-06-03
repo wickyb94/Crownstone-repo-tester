0db32d8280ebfc71e929805abcc0e02e6c50c2bf
wickyb94 <wicky_bhaggoe@hotmail.com>
2020-06-03 22:54:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from jlink import *
 from relaisiopi import *
-from multiplexer import *
+from multiplexer import Multiplexer
 from uarttest import *
+import math
 
 #The main script excecutes the tests and functions of the other scripts
 # These are the pins S0,S1,S2,S3,EN of the multiplexer connected to the Raspberry PI
@@ -15,11 +16,18 @@ bluenet.initializeUSB("/dev/ttyUSB0")
 
 ob = Programmer()
 commands = JLink(ob)
+multiplexers = []
+crownstoneCount = 3
+crownstonePerMultiplexer = 16
 
-for select in range(1, 3):
+multiplexer1 = Multiplexer (mp[0], mp[1], mp[2], mp[3], mp[4])
+multiplexers.append(multiplexer1)
+
+for select in range(1, crownstoneCount):
     setRelayOn(select)
-
-    multiplex(select, mp[0], mp[1], mp[2], mp[3], mp[4])
+    
+    multiplexerId = math.floor(select / crownstonePerMultiplexer)
+    multiplexers[multiplexerId].switch (select)
 
     jlinktupp = commands.run_filename('/home/pi/firmware-tester/loaddevfirmware.script', 20)
     jlinkout = jlinktupp.split('3.300V')[2]
