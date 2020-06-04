bdaf7e1bab5dcc0aa29a2d14ae27f0513efc34b8
wickyb94 <wicky_bhaggoe@hotmail.com>
2020-06-04 02:05:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ from multiplexer import Multiplexer
 from uarttest import *
 import math
 
-#The main script excecutes the tests and functions of the other scripts
-# initializing the pins of the multiplexer these pins are S0,S1,S2,S3,EN of the multiplexer
+# The main script excecutes the tests and functions of the other scripts
+# Initializing the pins of the multiplexer these pins are S0,S1,S2,S3,EN of the multiplexer
 mp = [6, 13, 19, 26, 21]
 GPIO.setmode (GPIO.BCM)
 for setting in range (0, 5):
     GPIO.setup (mp[setting], GPIO.OUT)
 
-#intitializing the usb port wich will be used for the bluenet library 
+# Intitializing the usb port wich will be used for the bluenet library 
 bluenet.initializeUSB ("/dev/ttyUSB0")
 
+# setting up how many crownstones are used and weet niet hoe ik de rest moet uitleggen 
+crownstoneCount = 2
 ob = Programmer ()
 commands = JLink (ob)
-multiplexers = []
-crownstoneCount = 2
 crownstonePerMultiplexer = 16
-
+multiplexers = []
 multiplexer1 = Multiplexer (mp[0], mp[1], mp[2], mp[3], mp[4])
 multiplexers.append (multiplexer1)
 
