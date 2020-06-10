30b6a8714ccdb36601f9f6175c8f9fc55260bd38
wickyb94 <wicky_bhaggoe@hotmail.com>
2020-06-10 23:20:44 +0200

    Update relaisiopi.py

--- relaisiopi.py
+++ relaisiopi.py
@@ -1,7 +1,7 @@
 from IOPi import IOPi
 import time
 
-#setting up the busses of the IO-extenssion board to control the relay module
+#setting up the busses of the IO-extenssion board to control the relay module board
 bus1 = IOPi(0x20)
 bus1.set_port_direction(0, 0x00)
 bus1.set_port_direction(1, 0x00)
@@ -14,33 +14,70 @@ bus2.set_port_direction(1, 0x00)
 bus2.write_port(0, 0xFF)
 bus2.write_port(1, 0xFF)
 
-#Function where the user can turn on the choosen relay there are 32 to choose from.
-def setRelayOn(relaynumber):
-    if relaynumber > 0 and relaynumber < 17:
-        bus1.write_pin(relaynumber, 0)
-    elif relaynumber > 16 and relaynumber < 33:
-        bus2.write_pin((relaynumber-16), 0)
-    else:
-        raise Exception ('relaynumber out of bounds, relaynumber must be an integer from 1 to 32')
+class Relayboards:
+    relayboards = []
+    relayboard1 = Relayboard (bus1)
+    relayboard2 = Relayboard (bus2)
+    relayboards.append (relayboard1)
+    relayboards.append (relayboard2)
+    
+    def setRelayOn (relaynumber):
+        relayId = math.floor (relaynumber / 16)
+        if 0 =< relaynumber =< 31:
+            if relayId == 0:
+                relaynumber = relaynumber + 1
+                relayboards[relayId].sellectRelayOn (relaynumber)
+            elif multiplexerId == 1:
+                relaynumber = relaynumber - 15
+                relayboards[relayId].sellectRelayOn (relaynumber)
+            else:
+                raise Exception ('relaynumber out of bounds, relaynumber must be an integer from 0 to 31')
+    
+    #Function that alows the user to turn on all the relays. For safety reason the relays wont turn simultaneously. 
+    #The function is used to test if the relayboard works
+    def setAllRelayOn():
+        for relaynumber in range(0, 33):
+            setRelayOn(relaynumber)
+            time.sleep(0.5)
 
-#Function where the user can turn off the choosen relay there are 32 to choose from.
-def setRelayOff(relaynumber):
-    if relaynumber > 0 and relaynumber < 17:
-        bus1.write_pin(relaynumber, 1)
-    elif relaynumber > 16 and relaynumber < 33:
-        bus2.write_pin((relaynumber - 16), 1)
-    else:
-        raise Exception ('relaynumber out of bounds, relaynumber must be an integer from 1 to 32')
+    #Function that alows the user to turn off all the relays. 
+    #The function is used to test if the relayboard works
+    def setAllRelayOff():
+        for relaynumber in range(0, 33):
+            setRelayOff(relaynumber)               
+     def setRelayOff (relaynumber):
+            relayId = math.floor (relaynumber / 16)
+            if 0 =< relaynumber =< 31:
+                if relayId == 0:
+                    relaynumber = relaynumber + 1
+                    relayboards[relayId].sellectRelayOff (relaynumber)
+                elif multiplexerId == 1:
+                    relaynumber = relaynumber - 15
+                    relayboards[relayId].sellectRelayOff (relaynumber)
+                else:
+                    raise Exception ('relaynumber out of bounds, relaynumber must be an integer from 0 to 31')
+
+    class Relayboard:
+        def __init__ (self, bus):
+            self.bus = bus
+
+        #Function where the user can turn on the choosen relay there are 32 to choose from.
+        def sellectRelayOn(self, relaynumber):
+            if 0 >= relaynumber =< 15:
+                relaynumber = relaynumber + 1
+                self.bus.write_pin(relaynumber, 0)
+            else:
+                raise Exception ('relaynumber out of bounds, relaynumber must be an integer from 0 to 15')
+
+        #Function where the user can turn off the choosen relay there are 32 to choose from.
+        def sellectRelayOff(self, relaynumber):
+            if 0 >= relaynumber =< 15:
+                relaynumber = relaynumber + 1
+                self.bus.write_pin(relaynumber, 1)
+            else:
+                raise Exception ('relaynumber out of bounds, relaynumber must be an integer from 0 to 15')
+
+
+
         
-#Function that alows the user to turn on all the relays. For safety reason the relays wont turn simultaneously. 
-#The function is used to test if the relayboard works
-def setAllRelayOn():
-    for relaynumber in range(0, 33):
-        setRelayOn(relaynumber)
-        time.sleep(0.5)
-        
-#Function that alows the user to turn off all the relays. 
-#The function is used to test if the relayboard works
-def setAllRelayOff():
-    for relaynumber in range(0, 33):
-        setRelayOff(relaynumber)
+
