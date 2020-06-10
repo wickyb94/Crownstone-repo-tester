98309ad58bd554390df9d894c6ef7c87d834fcc4
wickyb94 <wicky_bhaggoe@hotmail.com>
2020-06-10 22:34:32 +0200

    Update multiplexer.py

--- multiplexer.py
+++ multiplexer.py
@@ -11,7 +11,7 @@ for setting in range (0, 5):
 for setting in range (0, 5):
     GPIO.setup (mp2[setting], GPIO.OUT)
       
-class Multiplexers:
+class Multiplexers():
    multiplexers = []
    multiplexer1 = Multiplexer (mp1[0], mp1[1], mp1[2], mp1[3], mp1[4])
    multiplexer2 = Multiplexer (mp2[0], mp2[1], mp2[2], mp2[3], mp2[4])
@@ -35,7 +35,7 @@ class Multiplexers:
                time.sleep (0.1)
          
    # The multiplexer allows to switch between the different channels of the multiplexer 
-   class Multiplexer:
+   class Multiplexer (S0, S1, S2, S3, EN):
       # List of the 16 channels 
        muxChannel = [
                    [0, 0, 0, 0],
@@ -76,7 +76,7 @@ class Multiplexers:
                raise Exception ('Channel number out of bounds, channel must be an integer from 0 to 15')
 
        #function to test all if all the channels of this multiplexer are still working
-       def testAllOutputs (self):
+       def testMultiplexerOutputs (self):
            for channel in range (0, 16):
                self.switch (channel)
                print (channel)
