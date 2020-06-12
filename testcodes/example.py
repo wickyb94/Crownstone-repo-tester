9c793072f0a257584836385464612b8d754d4ccf
wickyb94 <wicky_bhaggoe@hotmail.com>
2020-06-12 12:55:18 +0200

    main code nu alles toegevoegd loaddevfirmware en loadfactoryfirmware om het uploaden van hex makkelijker te maken

--- testcodes/example.py
+++ testcodes/example.py
@@ -1,11 +1,40 @@
-import subprocess
-from subprocess import Popen, PIPE
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+from pathlib import Path
 
-c = 'JLinkExe'
-session = subprocess.Popen([c, '-ExitonError', '1'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+fh = open('demo.txt', 'w')
+fh.write('hallo ik ben kees')
+fh.close()
 
-hallo = session.stdout.decode("utf-8")
+sender_email = 'wicky_bhaggoe@hotmail.com'
+rec_email = 'bart@crownstone.rocks'
+password = 'eeenpassword'
+subject = 'bijlage versturen'
+body = 'dit is de bijlage die verstuurd wordt'
 
-print("FAILED TO PROGRAM", hallo)
+msg = MIMEMultipart()
+msg['From'] = sender_email
+msg['To'] = rec_email
+msg['Subject'] = subject
 
-session.returncode
+msg.attach(MIMEText(body, 'plain'))
+
+filename = 'demo.txt'
+attachment = open(filename,'r')
+
+part = MIMEBase('application', "plain")
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment, filename= "demo.txt" ')
+
+msg.attach(part)
+
+server = smtplib.SMTP("smtp.live.com",587)
+server.starttls()
+server.login(sender_email, password)
+print('login was a succes')
+server.sendmail(sender_email, rec_email, msg.as_string())
+print('mail has been sent to', rec_email)
